c0a826000c632a2dbae233683cc852e18dbe1546
Joel Godfrey-Smith <>
2021-05-10 20:11:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from PIL import Image
+
+#run as cmd line tool with two arguments, folder with pics and folder to deposit converted pics
+
+img_fldr = sys.argv[1]
+output_fldr = sys.argv[2]
+
+
+#if new folder to deposite converted pics does not exist, create it
+
+#loop through image folder
+
+#convert to png
+
+#save to new folder
