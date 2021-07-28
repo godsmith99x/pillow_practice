5d57faa22b5606a5bcc92200ac9615a71a6c1c77
Joel Godfrey-Smith <66573128+godsmith99x@users.noreply.github.com>
2021-07-28 21:03:38 +0000

    Completed challenge

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ import sys
 import os
 from PIL import Image
 
-#run as cmd line tool with two arguments, folder with pics and folder to deposit converted pics
+'''
+Run as cmd line tool with two arguments, folder with pics and folder to deposit converted pics
+'''
 
 img_fldr = sys.argv[1]
 output_fldr = sys.argv[2]
 
-#if new folder to deposit converted pics does not exist, create it
-
 if not os.path.exists(output_fldr):
     os.mkdir(output_fldr)
-
-#loop through image folder
+'''
+If new folder to deposit converted pics does not exist, create it
+'''
 
 for pic in os.listdir(img_fldr):
-    img = Image.open(pic)
-    img.save(f"{output_fldr}/{pic}.png", "png")
-
-#convert to png
-
-#save to new folder
+    img = Image.open(f"{img_fldr}/{pic}")
+    pic_name = pic.replace(".jpg", ".png")
+    img.save(f"{output_fldr}/{pic_name}", "png")
+    '''
+    Loop through image folder
+    Convert to png
+    Save to new folder
+    '''
